fa70c1ec7cc1d35022759b47613eba09e212a3c4
afdaza27 <af.daza@uniandes.edu.co>
2020-12-30 13:48:08 -0500

    JaBria

---
+++ cbot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+sapo = commands.Bot(command_prefix=">")
+
+@sapo.event
+async def on_ready():
+    print("Ready to torture some cock and balls")
+
+sapo.run("NzkzOTAyMzk2OTA3NTg1NTg2.X-zBOQ.bzS_KgeWr4WKhwP57DGKc9Howk8")
