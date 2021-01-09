459dbef5efdf245296ba7c19e09aa6661455a425
Max Planck <lj.garciam1@uniandes.edu.co>
2021-01-08 21:28:35 -0500

    Lonzo BALL

--- cbot.py
+++ cbot.py
@@ -8,7 +8,7 @@ import shutil
 from modulos.insultos import Insultos
 
 sapo = commands.Bot(command_prefix=">")
-BOT_KEY = "NzkzOTAyMzk2OTA3NTg1NTg2.X-zBOQ.JeObkoRjYG97hEdls0_oHCfaH4U"#environ["BOT_KEY"]
+BOT_KEY = environ["BOT_KEY"]
 
 
 async def check_mod(cbt):
