34f62f032309082df28cec320d19f0c5f7a05f85
Porygon Z and Eye of Truth <lj.garciam1@uniandes.edu.co>
2021-06-21 20:02:52 -0500

    patricio golo (negro)

--- cogotes/sabiduria.py
+++ cogotes/sabiduria.py
@@ -12,6 +12,7 @@ class Sabiduria(commands.Cog):
         self.Insultos.cargar_insultos()
         self.sapo = sapo
         self.adagios = self.cargar_adagios()
+        self.palabrotas = self.cargar_palabrotas()
         self.ego = "La vida de un crítico es sencilla en muchos aspectos, arriesgamos poco y tenemos poder sobre aquellos que ofrecen su trabajo \n  y su servicio a nuestro juicio. Prosperamos con las críticas negativas, divertidas de escribir y de leer, pero la triste verdad que \n debemos afrontar es que en el gran orden de las cosas, cualquier basura tiene más significado que lo que deja ver nuestra crítica. \n Pero en ocasiones el crítico si se arriesga cada vez que descubre y defiende algo nuevo, el mundo suele ser cruel con el \n nuevo talento; las nuevas creaciones, lo nuevo, necesita amigos. Anoche experimente algo nuevo; una extraordinaria cena de una \n fuente singular e inesperada. Decir solo que la comida y su creador han desafiado mis prejuicios sobre la buena cocina, \n subestimaría la realidad; me han tocado en lo más profundo. En el pasado jamás oculte mi desdén por el famoso lema del chef Gusteau: \n Cualquiera puede cocinar. Pero al fin me doy cuenta de lo que quiso decir en realidad; no cualquiera puede \n convertirse en un gran artista, pero un gran artista puede provenir de cualquier lado. Es difícil imaginar un origen más humilde que\n  el del genio que ahora cocina en el restaurante Gusteau´s y quien, en opinión de este critico, es nada menos que el \n mejor chef de Francia. ¡Pronto volveré a Gusteau´s hambriento!"
 
 
@@ -27,6 +28,14 @@ class Sabiduria(commands.Cog):
         for adagio in adagios:
             adagios_list.append(adagio.val())
         return adagios_list
+
+    def cargar_palabrotas(self):
+        user = cbot.sign_in()
+        palabrotas = self.db.child("Palabrotas").get(user["idToken"]).each()
+        plist = {}
+        for sapo in palabrotas:
+            plist[sapo.key()] = sapo.val()
+        return plist
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -53,6 +62,31 @@ class Sabiduria(commands.Cog):
                 await cbt.send("El adagio ya está registrado, "+self.Insultos.insultar())
         else:
             await cbt.send("Y a USTED quién le dió permiso?? "+self.Insultos.insultar())
+
+    @commands.command(brief="Una migaja de léxico", description="Una migaja de léxico")
+    async def palabrota(self, cbt, sapoArg=""):
+        if(sapoArg==""):
+            numericio = random.randint(0, len(self.palabrotas.keys()) - 1)
+            await cbt.send(list(self.palabrotas)[numericio])
+        elif(sapoArg in self.palabrotas.keys()):
+            await cbt.send(sapoArg + ": " + self.palabrotas[sapoArg])
+        else:
+            await cbt.send("Esa palabra no la manejo <:cogote:755197902049116201>")
+
+    @commands.command(brief="Agrega una nueva palabrota", description="Agrega una nueva palabrota junto a su definición, cada una de estas debe estar entre comillas.")
+    async def agregar_palabrota(self, cbt, palabrota = "", dlmao = ""):
+        user = cbot.sign_in()
+        if await cbot.check_mod(cbt) or str(cbt.author.id)=="599753171400786136":
+            if palabrota == "" or dlmao == "":
+                await cbt.send("Use el comando bien, "+self.Insultos.insultar())
+            elif palabrota not in self.palabrotas.keys():
+                self.palabrotas[palabrota] = dlmao
+                self.db.child("Palabrotas").set(self.palabrotas, user["idToken"])
+                await cbt.send("Palabrota agregada con éxito")
+            else:
+                await cbt.send("La palabrota ya está registrada, " + self.Insultos.insultar())
+        else:
+            await cbt.send("Y a USTED quién le dió permiso?? " + self.Insultos.insultar())
 
 
 def setup(sapo):
